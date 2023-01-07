bd0c32c0107df37bd334f0c93fd7b827b3dd9597
Theo <poette.theo@gmail.com>
2023-01-06 19:31:29 -0500

    fix: react 18 error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
 import "../styles/globals.css";
 import Layout from "./components/Layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* @ts-ignore */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 };
 
