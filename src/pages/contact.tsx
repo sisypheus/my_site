dcdb7132168ae1bf7bba70fbc1cfb309c0e0b279
Theo <poette.theo@gmail.com>
2023-01-19 15:52:49 -0500

    feat: contact page done

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,11 +5,18 @@ import FadeIn from "../components/FadeIn";
 import Image from "next/image";
 import Script from "next/script";
 import Alert from "../components/Alert";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import CopyIcon from "../components/CopyIcon";
 
 const Contact: NextPage = () => {
   const [error, setErrror] = useState("");
   const [success, setSuccess] = useState("");
+  const emailRef = useRef<any>(null);
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText(emailRef.current.textContent);
+    setSuccess("Email copied to clipboard");
+  };
 
   return (
     <>
@@ -26,9 +33,25 @@ const Contact: NextPage = () => {
 
         <ContactForm setSuccess={setSuccess} setError={setErrror} />
 
+        <div className="mt-8 flex flex-col items-center justify-center">
+          <h1 className="text-xl tracking-tight text-white">
+            Or send me an email from your preferred app
+          </h1>
+
+          <div
+            className="flex cursor-pointer items-center justify-center space-x-4 rounded-lg border border-gray-700 p-2 drop-shadow-2xl"
+            onClick={copyEmail}
+          >
+            <h1 ref={emailRef} className="text-white">
+              poette.theo@gmail.com
+            </h1>
+            <CopyIcon />
+          </div>
+        </div>
+
         <div className="mt-8">
           <h1 className="text-xl font-medium tracking-wide text-white underline underline-offset-4">
-            Or connect with me on
+            Connect with me on
           </h1>
 
           <div className="mt-8 flex items-center justify-center space-x-4">
