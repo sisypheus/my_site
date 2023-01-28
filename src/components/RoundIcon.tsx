f4103e7a628fa34f11c4519cf52731a128b85d81
Theo <poette.theo@gmail.com>
2023-01-28 13:58:40 -0500

    fix: translation and homepage animation

--- src/components/RoundIcon.tsx
+++ src/components/RoundIcon.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const RoundIcon = ({ icon }: { icon: string }) => {
   return (
-    <div className="flex items-center justify-center rounded-full bg-white h-8 w-8 sm:h-12 sm:w-12">
-      <Image src={icon} alt="icon" width={24} height={24} className="p-px" />
+    <div className="flex items-center justify-center rounded-full bg-white h-7 w-7 sm:h-12 sm:w-12">
+      <Image src={icon} alt="icon" width={24} height={24} className="p-1 sm:p-px" />
     </div>
   );
 };
